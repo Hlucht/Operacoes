de550384ba15e0cfb32d42b6f72964339618447b
Hlucht <luchtembergh@gmail.com>
2022-10-20 09:06:17 -0300

    Adicionando arquivos

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+const numero1 = document.getElementById('n1');
+const numero2 = document.getElementById('n2');
+const resultado = document.getElementById('resultado');
+const button_soma = document.getElementById('somar');
+const button_subtracao = document.getElementById('subtrair');
+const button_multiplicacao = document.getElementById('multiplicar');
+const button_divisao = document.getElementById('dividir');
+
+function soma(){
+    const total = parseInt(numero1.value) + parseInt(numero2.value);
+    resultado.innerText = `Resultado: ${total}`;
+}
+
+function subtracao(){
+        const total = parseInt(numero1.value) - parseInt(numero2.value);
+        resultado.innerText = `Resultado: ${total}`;
+}
+
+function multiplicacao(){
+    const total = parseInt(numero1.value) * parseInt(numero2.value);
+    resultado.innerText = `Resultado: ${total}`;
+}
+
+function divisao(){
+    if(numero1.value > 0){
+        const total = parseInt(numero1.value) / parseInt(numero2.value);
+        resultado.innerText = `Resultado: ${total}`;
+    }else{
+        alert(" Resultado Indeterminado ");
+    }
+}
+
+button_soma.addEventListener('click', soma);
+button_subtracao.addEventListener('click', subtracao);
+button_multiplicacao.addEventListener('click', multiplicacao);
+button_divisao.addEventListener('click', divisao);
+});
